f5bb8022da686a3c763dcb9ae82fd1f94cc5b828
rlacodmskim <112742002+rlacodmskim@users.noreply.github.com>
2022-09-27 21:13:44 +0900

    calibration for x,y,z
    z값이 0만 나옴, 띄어쓰기가 안 됨

--- 보정 자이로 (149).ino
+++ 보정 자이로 (149).ino
@@ -16,30 +16,44 @@ void loop() {
   Wire.beginTransmission(0x68);
   Wire.write(0x45);
   Wire.endTransmission(false);
-  Wire.requestFrom(0x68,2,true);
+  Wire.requestFrom(0x68,6,true);
+  int16_t GyXH = Wire.read();  
+  int16_t GyXL = Wire.read();
   int16_t GyYH = Wire.read();  
   int16_t GyYL = Wire.read();
-  int16_t GyY = GyYH <<8 |GyYL;
+  int16_t GyZH = Wire.read();  
+  int16_t GyZL = Wire.read();
+  int16_t GyX = GyXH <<8 |GyXL;
+  int16_t GyY = GyYH <<8 |GyYL; 
+  int16_t GyZ = GyZH <<8 |GyZL; //raw 값
 
-  static int32_t GyYSum =0;
-  static double GyYOff =0.0;
+  static int32_t GyXSum =0, GyYSum =0, GyZSum =0;
+  static double GyXOff =0.0, GyYOff =0.0, GyZOff =0.0;
   static int cnt_sample =1000;
   if(cnt_sample >0) {
-	GyYSum += GyY;
+	GyXSum += GyX, GyYSum += GyY, GyZSum += GyZ;
 	cnt_sample --;
 	if(cnt_sample ==0) {      
-	  GyYOff = GyYSum /1000.0;
+	  GyXOff = GyXSum /1000.0;
+    GyYOff = GyYSum /1000.0;
+    GyZOff = GyZSum /1000.0;  //GyY를 1000번 더한 값인 GyYSum을 이용해 GyY의 평균값인 GyYOff 구하기
 	}
-	delay(1);
+	delay(1); //1mm delay 후 return
 	return;    
   }
+  double GyXD = GyX - GyXOff;
   double GyYD = GyY - GyYOff;
+  double GyZD = GyZ - GyZOff; //보정된 값
 
   static int cnt_loop;
   cnt_loop ++;
-  if(cnt_loop%100 !=0) return;
+  if(cnt_loop%100 !=0) return;  //100번 마다 세기
 
- //  Serial.print("GyY = "); Serial.print(GyY);
+ /*  Serial.print("GyX = "); Serial.print(GyX), 
+     Serial.print("GyY = "); Serial.print(GyY), 
+     Serial.print("GyZ = "); Serial.print(GyZ); */
+  Serial.print("GyXD = "); Serial.print(GyXD);
   Serial.print("GyYD = "); Serial.print(GyYD);
+  Serial.print("GyZD = "); Serial.print(GyZD);
   Serial.println();
 }
